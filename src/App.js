e5584871e49b7ba3e895f308c10dd61bc3ec11fc
b729 <mowrey729@gmail.com>
2021-06-29 13:47:33 -0500

    Base React components initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from './HEADER';
+import './App.css';
+
+
+class App extends Component {
+  render(){
+   return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+}  
+
+export default App;
---
+++ src/HEADER.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <header>
+            <i className="fas fa-book-open fa-2x"></i>
+            <h1>Google Book Search</h1>
+        </header>
+    )
+}
+
+export default Header;
