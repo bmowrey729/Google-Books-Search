2d06eb26ddd59111135421d88a5861e5420e18ec
b729 <mowrey729@gmail.com>
2021-07-01 14:11:03 -0500

    API call working getting data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react';
-import Header from './HEADER';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Blog from "./components/pages/Blog";
+import Contact from "./components/pages/Contact";
 
-
-class App extends Component {
-  render(){
-   return (
-    <div className="App">
-      <Header />
-    </div>
+function App() {
+  return (
+    <Router>
+      <div>
+        <NavTabs />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/blog" component={Blog} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
   );
 }
-}  
 
 export default App;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,55 @@
+
+import React, { useState } from "react";
+
+import axios from 'axios';
+
+function App() {
+
+  const [book,setBook] = useState("");
+  const [result,setResult] = useState([]);
+  
+  function handleChange(event){
+
+    const book = event.target.value;
+
+    setBook(book);
+
+  }
+
+  function handleSubmit(event){
+
+    event.preventDefault();
+
+    axios.get("https://www.googleapis.com/books/v1/volumes?q=" +book+"&maxResults=40")
+      .then(data => {
+        console.log(data.data.items)
+        setResult(data.data.items)
+      })
+
+    
+  }
+
+  return (
+    <div className="container">
+      <h1>Google Book Search App</h1>
+      <form onSubmit={handleSubmit}> 
+        <div className= "form-group">
+          <input type= "text" onChange={handleChange}
+          className= "form-control mt-10" 
+          placeholder= "Search Google for Books" 
+          autoComplete= "off"/>
+        </div>
+        <button type= "submit" className= "btn btn-danger">Search</button>
+      </form>
+
+      {result.map(book => (
+        <a target= "_blank" href={book.volumeInfo.previewLink}>
+        <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title}/>
+        </a>
+      ))}
+      
+    </div>
+  );
+}
+
+export default App;
